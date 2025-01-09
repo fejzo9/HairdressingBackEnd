dc8f7434b878cb88bda885eebba1561574dce67a
fejzo9 <fejzo999@gmail.com>
2025-01-09 11:39:33 +0100

    In LoginController class for response, changed from String to be JSON object, with message and parameters

--- src/main/java/com/hairbooking/reservation/controller/LoginController.java
+++ src/main/java/com/hairbooking/reservation/controller/LoginController.java
@@ -6,6 +6,9 @@ import com.hairbooking.reservation.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/login")
 public class LoginController {
@@ -17,7 +20,7 @@ public class LoginController {
     }
 
     @PostMapping
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         String identifier = loginRequest.getIdentifier();
         String password = loginRequest.getPassword();
 
@@ -26,8 +29,11 @@ public class LoginController {
         if (user == null || !userService.verifyPassword(user, password)) {
             return ResponseEntity.status(401).body("Invalid username/email or password");
         }
-
-        return ResponseEntity.ok("Login successful!");
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Login successful!");
+        response.put("firstName", user.getUsername());
+        response.put("lastName", user.getLastName());
+        return ResponseEntity.ok(response);
     }
 
 }
