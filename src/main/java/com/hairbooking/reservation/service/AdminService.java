fa212f82ee06a77caba4898160e6e5328d416c46
fejzo9 <fejzo999@gmail.com>
2025-01-11 08:09:30 +0100

    In AdminService class added a logic for crypting password and saving crypted password into database

--- src/main/java/com/hairbooking/reservation/service/AdminService.java
+++ src/main/java/com/hairbooking/reservation/service/AdminService.java
@@ -2,18 +2,22 @@ package com.hairbooking.reservation.service;
 
 import com.hairbooking.reservation.model.Admin;
 import com.hairbooking.reservation.repository.AdminRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
 
     private final AdminRepository adminRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public AdminService(AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public Admin saveAdmin(Admin admin) {
+        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
         return adminRepository.save(admin);
     }
 
