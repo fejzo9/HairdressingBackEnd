85eb4646905789157f68e1d304756a1171236266
fejzo9 <fejzo999@gmail.com>
2025-01-11 07:16:18 +0100

    Created AdminRepository class inside repository folder

---
+++ src/main/java/com/hairbooking/reservation/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.hairbooking.reservation.repository;
+
+import com.hairbooking.reservation.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByUsername(String username);
+}
