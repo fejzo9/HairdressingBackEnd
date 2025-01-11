4a40d44458a9aabacebd8a0af3ebf4ea1222e20f
fejzo9 <fejzo999@gmail.com>
2025-01-11 09:45:38 +0100

    In SecurityConfig class added line of code for cors and for /admins endpoint to get access with ADMIN role

--- src/main/java/com/hairbooking/reservation/config/SecurityConfig.java
+++ src/main/java/com/hairbooking/reservation/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +16,17 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http
                 .csrf(AbstractHttpConfigurer::disable) // Privremeno isključivanje CSRF zaštite za testiranje
-                .authorizeHttpRequests(auth -> auth
+                    .cors(Customizer.withDefaults())
+                    .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/registration/**", "/users/**", "/login/**").permitAll() // Omogućiti pristup za POST na /users i svim podputanjama
+                        .requestMatchers("/admins/**").hasRole("ADMIN")
                         .anyRequest().authenticated() // Svi ostali zahtjevi zahtijevaju autentifikaciju
                 ).cors(Customizer.withDefaults());
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
