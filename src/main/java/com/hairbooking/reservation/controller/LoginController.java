0709ac5791fb05191290a9263214ff935d1ba34b
fejzo9 <fejzo999@gmail.com>
2025-01-13 22:28:18 +0100

    In LoginController class, added adminService field so when we're logging it could logg also with USER role and ADMIN role

--- src/main/java/com/hairbooking/reservation/controller/LoginController.java
+++ src/main/java/com/hairbooking/reservation/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.hairbooking.reservation.controller;
 
+import com.hairbooking.reservation.model.Admin;
 import com.hairbooking.reservation.model.LoginRequest;
 import com.hairbooking.reservation.model.User;
+import com.hairbooking.reservation.service.AdminService;
 import com.hairbooking.reservation.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +16,12 @@ import java.util.Map;
 public class LoginController {
 
     private final UserService userService;
+    private final AdminService adminService;
 
-    public LoginController(UserService userService) {
+    public LoginController(UserService userService, AdminService adminService) {
+
         this.userService = userService;
+        this.adminService = adminService;
     }
 
     @PostMapping
@@ -25,16 +30,24 @@ public class LoginController {
         String password = loginRequest.getPassword();
 
         User user = userService.findByUsernameOrEmail(identifier);
+        Admin admin = adminService.findByUsernameOrEmail(identifier);
 
         if (user == null || !userService.verifyPassword(user, password)) {
-            return ResponseEntity.status(401).body("Invalid username/email or password");
+            if (admin == null || !adminService.verifyPassword(admin, password)) {
+                return ResponseEntity.status(401).body("Invalid username/email or password");
+            }
         }
+
         Map<String, String> response = new HashMap<>();
         response.put("message", "Login successful!");
-        response.put("firstName", user.getUsername());
-        response.put("lastName", user.getLastName());
+        if(user != null){
+             response.put("firstName", user.getUsername());
+             response.put("lastName", user.getLastName());
+        }
+        if(admin != null){
+            response.put("firstName", admin.getUsername());
+        }
         return ResponseEntity.ok(response);
     }
-
 }
 
