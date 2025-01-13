891f2aa5458663e68c88adfe85b9902c986a861a
fejzo9 <fejzo999@gmail.com>
2025-01-13 21:58:49 +0100

    In UserRepository class added methods findByEmail() and findAllByRole()

--- src/main/java/com/hairbooking/reservation/repository/UserRepository.java
+++ src/main/java/com/hairbooking/reservation/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package com.hairbooking.reservation.repository;
 
+import com.hairbooking.reservation.model.Role;
 import com.hairbooking.reservation.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    List<User> findAllByRole(Role role);
 }
