5a6a1857a30ed0360b845a8be0f7e25c9f961fc7
fejzo9 <fejzo999@gmail.com>
2025-01-13 22:08:05 +0100

    In AdminService class modified class findByUsername, and name of the method changed to findByUsernameOrEmail() and called method from repo. Also added method for verification password of admin

--- src/main/java/com/hairbooking/reservation/service/AdminService.java
+++ src/main/java/com/hairbooking/reservation/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.hairbooking.reservation.service;
 
 import com.hairbooking.reservation.model.Admin;
+import com.hairbooking.reservation.model.User;
 import com.hairbooking.reservation.repository.AdminRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,8 +24,9 @@ public class AdminService {
         return adminRepository.save(admin);
     }
 
-    public Admin findByUsername(String username) {
-        return adminRepository.findByUsername(username);
+    public Admin findByUsernameOrEmail(String identifier) {
+        return adminRepository.findByUsernameOrEmail(identifier, identifier)
+                .orElse(null);
     }
 
     public List<Admin> getAllAdmins() {
@@ -46,5 +48,10 @@ public class AdminService {
         Admin admin = adminRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Admin not found with ID: " + id));
         adminRepository.delete(admin);
+    }
+
+    // Verifikacija lozinke
+    public boolean verifyPassword(Admin admin, String rawPassword) {
+        return passwordEncoder.matches(rawPassword, admin.getPassword());
     }
 }
