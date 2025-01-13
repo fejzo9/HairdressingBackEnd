402bdea976d2000a2f688981e919fb257bc26905
fejzo9 <fejzo999@gmail.com>
2025-01-13 22:03:51 +0100

    In Admin Repository interface added method findByUsernameOrEmail() so the user could be find also by email, not just un

--- src/main/java/com/hairbooking/reservation/repository/AdminRepository.java
+++ src/main/java/com/hairbooking/reservation/repository/AdminRepository.java
@@ -4,7 +4,11 @@ import com.hairbooking.reservation.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Admin findByUsername(String username);
+    Optional<Admin> findByUsernameOrEmail(String username, String email);
 }
+
+
