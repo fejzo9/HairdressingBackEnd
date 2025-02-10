945213d85b7908444cc2dc716274abd096baf774
fejzo9 <fejzo999@gmail.com>
2025-02-10 10:04:55 +0100

    In UserRepository added method findById, because I need it in SalonController

--- src/main/java/com/hairbooking/reservation/repository/UserRepository.java
+++ src/main/java/com/hairbooking/reservation/repository/UserRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     List<User> findAllByRole(Role role);
