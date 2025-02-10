97b8fbdb5115c9c3638b0b2ac09c36bbc3030c4a
fejzo9 <fejzo999@gmail.com>
2025-02-10 10:07:35 +0100

    In User class override methods equals and hashCode, because deleting employee was not working

--- src/main/java/com/hairbooking/reservation/model/User.java
+++ src/main/java/com/hairbooking/reservation/model/User.java
@@ -168,4 +168,17 @@ public class User {
     public Role getRole() { return role; }
 
     public void setRole(Role role) { this.role = role; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return id != null && id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
