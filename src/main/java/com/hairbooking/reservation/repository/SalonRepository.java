09b95efaface5aaa73ca57304b9c38d76cc4e3de
fejzo9 <fejzo999@gmail.com>
2025-02-12 08:50:16 +0100

    In SalonRepository added method findById() with @EntityGraph annotation. With this I fixed " LazyInitializationException" error that I was having.

--- src/main/java/com/hairbooking/reservation/repository/SalonRepository.java
+++ src/main/java/com/hairbooking/reservation/repository/SalonRepository.java
@@ -1,12 +1,17 @@
 package com.hairbooking.reservation.repository;
 
 import com.hairbooking.reservation.model.Salon;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SalonRepository extends JpaRepository<Salon, Long> {
     List<Salon> findByOwnerId(Long ownerId);
+
+    @EntityGraph(attributePaths = {"images", "imageTypes"})
+    Optional<Salon> findById(Long id);
 }
