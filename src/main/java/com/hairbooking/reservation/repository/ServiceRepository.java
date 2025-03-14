afc68f4dfd492dea0039759772072ce9f9068379
fejzo9 <fejzo999@gmail.com>
2025-03-14 15:25:51 +0100

    In SalonRepository Changed className Service into ServiceInSalon

--- src/main/java/com/hairbooking/reservation/repository/ServiceRepository.java
+++ src/main/java/com/hairbooking/reservation/repository/ServiceRepository.java
@@ -1,12 +1,12 @@
 package com.hairbooking.reservation.repository;
 
-import com.hairbooking.reservation.model.Service;
+import com.hairbooking.reservation.model.ServiceInSalon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-    List<Service> findBySalonId(Long salonId);
+public interface ServiceRepository extends JpaRepository<ServiceInSalon, Long> {
+    List<ServiceInSalon> findBySalonId(Long salonId);
 }
