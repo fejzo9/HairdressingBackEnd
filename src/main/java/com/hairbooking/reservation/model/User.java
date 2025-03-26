89b3f27701725cdfa28a697a9a4255c655c9a4a2
fejzo9 <fejzo999@gmail.com>
2025-03-26 13:27:31 +0100

    From User class delete import that was not used about BCryptPasswordEncoder

--- src/main/java/com/hairbooking/reservation/model/User.java
+++ src/main/java/com/hairbooking/reservation/model/User.java
@@ -3,7 +3,6 @@ package com.hairbooking.reservation.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 
