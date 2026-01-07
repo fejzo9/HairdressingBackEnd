7aa6aeb568c0adfc0329abfec7998505b08acc9d
fejzo9 <fejzo999@gmail.com>
2026-01-07 23:11:59 +0100

    DatabaseTest fix pw and port number

--- src/main/java/database/DatabaseTest.java
+++ src/main/java/database/DatabaseTest.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseTest {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5433/hairdressingdb";
+        String url = "jdbc:postgresql://localhost:5432/hairdressingdb";
         String username = "postgres";
-        String password = "123321fz";
+        String password = "postgres";
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Connection successful!");
